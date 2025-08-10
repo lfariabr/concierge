fb885e571741832001925f6e76845338343c1532
lfariabr <49277028+lfariabr@users.noreply.github.com>
2025-08-10 08:18:23 +0000

    feat(page): title adjustment

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ from views import rosterView, deliveryView
 
 # ---- Streamlit Config ----
 st.set_page_config(
-    page_title="Concierge Dashboard",
+    page_title="Lift Finder",
     layout="wide",
     page_icon="🛎️",
     initial_sidebar_state="collapsed"
 )
 
 # ---- App Title ----
-st.title("Concierge Tools ")
-st.markdown("Select a tool from the sidebar to get started.")
+st.title("Concierge Tool")
+# st.markdown("Select a tool from the sidebar to get started.")
+st.markdown("Use the tool below to optimize your delivery process.")
 
 # ---- Sidebar Navigation ----
 menu = st.sidebar.radio("📂 Select a screen:", [
